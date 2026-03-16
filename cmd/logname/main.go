a41d882233df24d13f78f15c436a9d5f44f5f81f
Petar Djukic <petar.djukic@gmail.com>
2026-03-16 02:36:20 -0500

    Task 1755: [stitch] prd053-logname R1.1-R1.2, R2.1-R2.2: core logname implementation and version/help flags
    Tokens-Input: 478941
    Tokens-Output: 3244
    Tokens-Cache-Creation: 74034
    Tokens-Cache-Read: 404900
    Tokens-Cost-USD: 0.7463
    Loc-Prod-Before: 7577
    Loc-Prod-After: 7657
    Loc-Test-Before: 10156
    Loc-Test-After: 10253
    Duration-Seconds: 46

---
+++ cmd/logname/main.go
@@ -0,0 +1,80 @@
+// Copyright (c) 2026 Petar Djukic. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+// Implements prd053-logname R1.1-R1.2, R2.1-R2.2:
+// cmd/logname prints the login name of the current user.
+// Supports --help and --version flags. Rejects unknown flags and extra operands.
+// Installs SIGPIPE handler per ARCHITECTURE.yaml.
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+	"strings"
+
+	"github.com/petar-djukic/go-unix-utils/pkg/sys"
+	"github.com/petar-djukic/go-unix-utils/pkg/version"
+)
+
+func main() {
+	sys.InstallSIGPIPEHandler()
+
+	if err := parseArgs(os.Args[1:]); err != nil {
+		fmt.Fprintf(os.Stderr, "%s: %s\n", os.Args[0], err)             //nolint:errcheck // best-effort diagnostic
+		fmt.Fprintf(os.Stderr, "Try '%s --help' for more information.\n", os.Args[0]) //nolint:errcheck // best-effort diagnostic
+		os.Exit(1)
+	}
+
+	// R1.1-R1.2: print the login name followed by a newline.
+	u, err := user.Current()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s: no login name\n", os.Args[0]) //nolint:errcheck // best-effort diagnostic
+		os.Exit(1)
+	}
+
+	fmt.Println(u.Username)
+}
+
+// parseArgs validates command-line arguments. logname accepts only --help and
+// --version; any other flag or operand is an error.
+func parseArgs(args []string) error {
+	for _, arg := range args {
+		if arg == "--" {
+			continue
+		}
+
+		// R2.2: --help prints usage to stdout and exits 0.
+		if arg == "--help" {
+			fmt.Fprintf(os.Stdout, //nolint:errcheck // best-effort output
+				"Usage: %s [OPTION]\n"+
+					"Print the name of the current user.\n\n"+
+					"      --help     display this help and exit\n"+
+					"      --version  output version information and exit\n",
+				os.Args[0],
+			)
+			os.Exit(0)
+		}
+
+		// R2.1: --version prints version info to stdout and exits 0.
+		if arg == "--version" {
+			fmt.Fprintf(os.Stdout, "%s (%s) %s\n", //nolint:errcheck // best-effort output
+				os.Args[0], "go-unix-utils", version.Version,
+			)
+			os.Exit(0)
+		}
+
+		// R2.2: unknown flags produce an error.
+		if strings.HasPrefix(arg, "--") {
+			return fmt.Errorf("unrecognized option '%s'", arg)
+		}
+		if strings.HasPrefix(arg, "-") && len(arg) > 1 {
+			return fmt.Errorf("invalid option -- '%c'", arg[1])
+		}
+
+		// R2.1: extra operands produce an error.
+		return fmt.Errorf("extra operand '%s'", arg)
+	}
+
+	return nil
+}
