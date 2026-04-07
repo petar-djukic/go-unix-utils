b5703fa1775e797ae2d0619b941e3cd419078915
Petar Djukic <petar.djukic@gmail.com>
2026-04-06 20:01:38 -0400

    Task 4427: [stitch] pkg/sys: InstallSIGPIPEHandler and signal utilities -- srd002 R2.1, R2.2, R2.3, R2.4
    Tokens-Input: 95891
    Tokens-Output: 39
    Tokens-Cache-Creation: 560
    Tokens-Cache-Read: 95328
    Tokens-Cost-USD: 1.6715
    Loc-Prod-Before: 781
    Loc-Prod-After: 811
    Loc-Test-Before: 64
    Loc-Test-After: 141
    Duration-Seconds: 692

--- pkg/sys/sigpipe.go
+++ pkg/sys/sigpipe.go
@@ -5,7 +5,7 @@ package sys
 
 // InstallSIGPIPEHandler installs a SIGPIPE handler causing the process to
 // exit 0 when stdout or stderr is closed by a downstream consumer.
-// Safe to call multiple times. See srd002-sys R1.5, R1.6.
+// Safe to call multiple times; only one goroutine is started. See srd002-sys R1.5, R1.6.
 func InstallSIGPIPEHandler() {
-	panic("not implemented")
+	sigpipeOnce.Do(installSIGPIPEHandlerImpl)
 }
