ba89c4106a5f8a979b8a5cdfd1264004fe9edfa8
Petar Djukic <petar.djukic@gmail.com>
2026-04-08 08:07:42 -0400

    Task 4667: [stitch] cmd/unlink: implementation -- srd038 R1.1, R1.2, R1.3, R2.1
    Tokens-Input: 555282
    Tokens-Output: 1983
    Tokens-Cache-Creation: 60243
    Tokens-Cache-Read: 495030
    Tokens-Cost-USD: 0.6737
    Loc-Prod-Before: 13487
    Loc-Prod-After: 13537
    Loc-Test-Before: 10482
    Loc-Test-After: 10482
    Duration-Seconds: 49

---
+++ cmd/unlink/main.go
@@ -0,0 +1,50 @@
+// Copyright (c) 2026 Petar Djukic. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+// Package main implements cmd/unlink: remove a single file via unlink(2).
+// Implements srd038 R1.1, R1.2, R1.3, R2.1.
+package main
+
+import (
+	"fmt"
+	"os"
+	"syscall"
+
+	"github.com/petar-djukic/go-unix-utils/pkg/sys"
+)
+
+const programName = "unlink"
+
+// main entry point with SIGPIPE handler and argument validation.
+func main() {
+	sys.InstallSIGPIPEHandler()
+
+	args := os.Args[1:]
+
+	// R1.3, R2.1: exactly one argument required.
+	if len(args) != 1 {
+		printUsageError(args)
+		os.Exit(1)
+	}
+
+	// R1.1: call unlink(2) on the single argument.
+	if err := syscall.Unlink(args[0]); err != nil {
+		// R2.1: GNU format error to stderr.
+		fmt.Fprintf(os.Stderr, "%s: cannot unlink '%s': %s\n",
+			programName, args[0], err.Error())
+		os.Exit(1)
+	}
+	// R1.2: exit 0 on success, no stdout output.
+}
+
+// printUsageError prints the appropriate error for wrong argument count.
+func printUsageError(args []string) {
+	if len(args) == 0 {
+		fmt.Fprintf(os.Stderr, "%s: missing operand\n", programName)
+	} else {
+		fmt.Fprintf(os.Stderr, "%s: extra operand '%s'\n",
+			programName, args[1])
+	}
+	fmt.Fprintf(os.Stderr, "Try '%s --help' for more information.\n",
+		programName)
+}
