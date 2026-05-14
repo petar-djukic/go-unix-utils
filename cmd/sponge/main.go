2e588149bbc0feb3571ea8ca7cd9f94c32daec96
Petar Djukic <petar.djukic@gmail.com>
2026-05-13 21:24:13 -0400

    Task 5116: [stitch] cmd/sponge (srd007-sponge R1.1, R1.2, R1.3, R1.4)
    Tokens-Input: 398485
    Tokens-Output: 8790
    Tokens-Cache-Creation: 165030
    Tokens-Cache-Read: 233447
    Tokens-Cost-USD: 1.7081
    Loc-Prod-Before: 1441
    Loc-Prod-After: 1527
    Loc-Test-Before: 190
    Loc-Test-After: 190
    Duration-Seconds: 194

--- cmd/sponge/main.go
+++ cmd/sponge/main.go
@@ -7,7 +7,10 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"io"
 	"os"
+	"runtime"
 
 	"github.com/petar-djukic/go-unix-utils/pkg/sys"
 )
@@ -21,11 +24,94 @@ func main() {
 }
 
 func run() int {
+	data, tmpFile, err := readAllStdin()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "sponge: %v\n", err)
+		return 1
+	}
+	if tmpFile != "" {
+		defer cleanupTempFile(tmpFile)
+	}
+
+	args := flag.Args()
+	if len(args) == 0 {
+		if err := writeToStdout(data, tmpFile); err != nil {
+			fmt.Fprintf(os.Stderr, "sponge: %v\n", err)
+			return 1
+		}
+		return 0
+	}
+
+	filename := args[0]
+	if *appendMode {
+		data, err = prependOriginalContent(filename, data)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "sponge: %v\n", err)
+			return 1
+		}
+	}
+
+	if err := writeOutputFile(filename, data, tmpFile); err != nil {
+		fmt.Fprintf(os.Stderr, "sponge: %v\n", err)
+		return 1
+	}
 	return 0
 }
 
 func readAllStdin() ([]byte, string, error) {
-	return nil, "", nil
+	data, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		return nil, "", fmt.Errorf("read stdin: %w", err)
+	}
+	threshold := spillThreshold()
+	if uint64(len(data)) > threshold {
+		tmpPath, err := spillToTempFile(data)
+		if err != nil {
+			return nil, "", err
+		}
+		return nil, tmpPath, nil
+	}
+	return data, "", nil
+}
+
+func spillThreshold() uint64 {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	avail := m.Sys - m.HeapInuse
+	if avail == 0 {
+		return 256 * 1024 * 1024
+	}
+	quarter := avail / 4
+	const maxThreshold = 1024 * 1024 * 1024
+	if quarter > maxThreshold {
+		return maxThreshold
+	}
+	return quarter
+}
+
+func spillToTempFile(data []byte) (string, error) {
+	f, err := os.CreateTemp(tempDir(), "sponge.")
+	if err != nil {
+		return "", fmt.Errorf("create temp file: %w", err)
+	}
+	path := f.Name()
+	if _, err := f.Write(data); err != nil {
+		f.Close()
+		os.Remove(path)
+		return "", fmt.Errorf("write temp file: %w", err)
+	}
+	if err := f.Close(); err != nil {
+		os.Remove(path)
+		return "", fmt.Errorf("close temp file: %w", err)
+	}
+	return path, nil
+}
+
+func tempDir() string {
+	if d := os.Getenv("TMPDIR"); d != "" {
+		return d
+	}
+	return "/tmp"
 }
 
 func writeOutputFile(filename string, data []byte, tmpFile string) error {
