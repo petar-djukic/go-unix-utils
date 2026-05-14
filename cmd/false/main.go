4c7e4dfdc16c6e60bc636c83dbfed549b506a890
Petar Djukic <petar.djukic@gmail.com>
2026-05-13 22:36:08 -0400

    Task 5127: [stitch] cmd/false: implement false utility (srd014-false R1.1-R1.3, R2.1-R2.3)
    Tokens-Input: 1143416
    Tokens-Output: 2017
    Tokens-Cache-Creation: 152902
    Tokens-Cache-Read: 990506
    Tokens-Cost-USD: 1.6426
    Loc-Prod-Before: 1616
    Loc-Prod-After: 1661
    Loc-Test-Before: 683
    Loc-Test-After: 683
    Duration-Seconds: 56

---
+++ cmd/false/main.go
@@ -0,0 +1,45 @@
+// Copyright (c) 2026 Petar Djukic. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+// cmd/false exits with status 1, ignoring all arguments.
+// Implements srd014-false R1.1-R1.3, R2.1-R2.3, R3.1-R3.2.
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/petar-djukic/go-unix-utils/pkg/sys"
+)
+
+const helpText = `Usage: false [ignored command line arguments]
+  or:  false OPTION
+Exit with a status code indicating failure.
+
+      --help     display this help and exit
+      --version  output version information and exit
+`
+
+const versionText = `false (go-unix-utils) dev
+`
+
+func main() {
+	sys.InstallSIGPIPEHandler()
+
+	if len(os.Args) > 1 {
+		switch os.Args[1] {
+		case "--help":
+			if _, err := fmt.Fprint(os.Stdout, helpText); err != nil {
+				os.Exit(1)
+			}
+			os.Exit(0)
+		case "--version":
+			if _, err := fmt.Fprint(os.Stdout, versionText); err != nil {
+				os.Exit(1)
+			}
+			os.Exit(0)
+		}
+	}
+
+	os.Exit(1)
+}
