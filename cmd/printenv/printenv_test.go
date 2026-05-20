81d7d787f0dc3c681d3a514e9612768906b717e0
Petar Djukic <petar.djukic@gmail.com>
2026-05-20 10:20:37 -0400

    Task 5365: [stitch] srd040-printenv R1.1, R1.2, R1.3, R2.1
    Tokens-Input: 398947
    Tokens-Output: 2774
    Tokens-Cache-Creation: 206490
    Tokens-Cache-Read: 192445
    Tokens-Cost-USD: 1.8218
    Loc-Prod-Before: 11182
    Loc-Prod-After: 11259
    Loc-Test-Before: 11405
    Loc-Test-After: 11469
    Duration-Seconds: 107

---
+++ cmd/printenv/printenv_test.go
@@ -0,0 +1,64 @@
+// Copyright 2024 Petar Djukic. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package main_test
+
+import (
+	"os/exec"
+	"testing"
+
+	"github.com/petar-djukic/go-unix-utils/pkg/testutils"
+)
+
+func TestDiff(t *testing.T) {
+	goBin := testutils.BuildBinary(t, ".")
+
+	refBin, err := exec.LookPath("gprintenv")
+	if err != nil {
+		t.Skip("reference binary not found")
+	}
+
+	tests := []testutils.DiffTest{
+		{
+			Name: "no args full dump",
+		},
+		{
+			Name: "single existing variable",
+			Args: []string{"HOME"},
+		},
+		{
+			Name: "multiple existing variables",
+			Args: []string{"HOME", "PATH"},
+		},
+		{
+			Name:     "missing variable",
+			Args:     []string{"NONEXISTENT_VAR_FOR_PRINTENV_TEST"},
+			ExitCode: 1,
+		},
+		{
+			Name:     "mix of existing and missing variables",
+			Args:     []string{"HOME", "NONEXISTENT_VAR_FOR_PRINTENV_TEST"},
+			ExitCode: 1,
+		},
+		{
+			Name: "null terminated no args",
+			Args: []string{"-0"},
+		},
+		{
+			Name: "null terminated single variable",
+			Args: []string{"-0", "HOME"},
+		},
+		{
+			Name:     "null terminated missing variable",
+			Args:     []string{"-0", "NONEXISTENT_VAR_FOR_PRINTENV_TEST"},
+			ExitCode: 1,
+		},
+		{
+			Name: "long flag null",
+			Args: []string{"--null", "HOME"},
+		},
+	}
+
+	testutils.RunDiffTests(t, goBin, refBin, tests)
+}
