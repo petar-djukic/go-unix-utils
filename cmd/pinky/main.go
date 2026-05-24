a15a793b28ce57ae7218c60fcd98d37b99b73fef
Petar Djukic <petar.djukic@gmail.com>
2026-05-24 00:15:57 -0400

    Task 5573: [stitch] Implement cmd/pinky output formatting and flags (srd098 R2.1, R2.2, R2.3)
    Tokens-Input: 1578048
    Tokens-Output: 46188
    Tokens-Cache-Creation: 265218
    Tokens-Cache-Read: 1312804
    Tokens-Cost-USD: 3.8945
    Loc-Prod-Before: 26053
    Loc-Prod-After: 26167
    Loc-Test-Before: 22974
    Loc-Test-After: 22974
    Duration-Seconds: 907

--- cmd/pinky/main.go
+++ cmd/pinky/main.go
@@ -1,7 +1,7 @@
 // Copyright (c) 2026 Petar Djukic. All rights reserved.
 // SPDX-License-Identifier: MIT
 
-// Implements srd098-pinky R1.1, R1.2, R1.3.
+// Implements srd098-pinky R1.1, R1.2, R1.3, R2.1, R2.2, R2.3.
 package main
 
 /*
@@ -23,6 +23,11 @@ import (
 
 const helpText = `Usage: pinky [OPTION]... [USER]...
 
+  -l     produce long format output for specified USERs
+  -b     omit the user's home directory and shell in long format
+  -f     omit the line of column headings in short format
+  -h     omit the user's project file in long format
+  -p     omit the user's plan file in long format
   -s     do short format output, this is the default
       --help     display this help and exit
       --version  output version information and exit
@@ -34,6 +39,12 @@ The utmp file will be /var/run/utmpx.
 const versionText = `pinky (go-unix-utils) dev
 `
 
+var (
+	longFormat     bool
+	suppressHeader bool
+	suppressHomeSh bool
+)
+
 type utmpEntry struct {
 	user string
 	line string
@@ -41,9 +52,28 @@ type utmpEntry struct {
 	time int64
 }
 
+type passwdInfo struct {
+	name   string
+	office string
+	phone  string
+	dir    string
+	shell  string
+}
+
 func main() {
 	sys.InstallSIGPIPEHandler()
 	users := parseArgs(os.Args[1:])
+	if longFormat {
+		if len(users) == 0 {
+			fmt.Fprintln(os.Stderr, "pinky: no username specified; at least one must be specified when using -l")
+			fmt.Fprintln(os.Stderr, "Try 'pinky --help' for more information.")
+			os.Exit(1)
+		}
+		if err := printLong(users); err != nil {
+			os.Exit(1)
+		}
+		return
+	}
 	if err := printShort(users); err != nil {
 		os.Exit(1)
 	}
@@ -82,8 +112,16 @@ func parseArgs(args []string) []string {
 func parseFlagBundle(flags string) {
 	for _, ch := range flags {
 		switch ch {
+		case 'l':
+			longFormat = true
 		case 's':
-			// -s forces short format, which is the default.
+			longFormat = false
+		case 'f':
+			suppressHeader = true
+		case 'b':
+			suppressHomeSh = true
+		case 'h', 'p':
+			// Accepted; no-op on macOS (no .project/.plan files).
 		default:
 			fmt.Fprintf(os.Stderr, "pinky: invalid option -- '%c'\n", ch)
 			fmt.Fprintln(os.Stderr, "Try 'pinky --help' for more information.")
@@ -94,20 +132,22 @@ func parseFlagBundle(flags string) {
 
 func printShort(users []string) error {
 	entries := readEntries(users)
-	hdr := fmt.Sprintf("%-9s%-21s%-9s%-7s%-17s%s",
-		"Login", "Name", "TTY", "Idle", "When", "Where")
-	if _, err := fmt.Fprintln(os.Stdout, hdr); err != nil {
-		return err
+	if !suppressHeader {
+		hdr := fmt.Sprintf("%-9s%-21s%-9s%-7s%-17s%s",
+			"Login", "Name", "TTY", "Idle", "When", "Where")
+		if _, err := fmt.Fprintln(os.Stdout, hdr); err != nil {
+			return err
+		}
 	}
 	for _, e := range entries {
-		if err := printEntry(e); err != nil {
+		if err := printShortEntry(e); err != nil {
 			return err
 		}
 	}
 	return nil
 }
 
-func printEntry(e utmpEntry) error {
+func printShortEntry(e utmpEntry) error {
 	name := lookupGecos(e.user)
 	idle := idleString(e.line)
 	when := formatLoginTime(e.time)
@@ -118,6 +158,55 @@ func printEntry(e utmpEntry) error {
 	} else {
 		line = fmt.Sprintf("%-9s%-21s%-9s%-7s%s",
 			e.user, name, e.line, idle, when)
+	}
+	_, err := fmt.Fprintln(os.Stdout, line)
+	return err
+}
+
+func printLong(users []string) error {
+	for _, u := range users {
+		if err := printLongEntry(u); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func printLongEntry(user string) error {
+	pw := lookupPasswd(user)
+	name := "???"
+	if pw != nil {
+		name = pw.name
+	}
+	if _, err := fmt.Fprintf(os.Stdout, "Login name: %-28sIn real life:  %s\n", user, name); err != nil {
+		return err
+	}
+	if pw != nil && !suppressHomeSh {
+		if _, err := fmt.Fprintf(os.Stdout, "Directory: %-29sShell:  %s\n", pw.dir, pw.shell); err != nil {
+			return err
+		}
+	}
+	if pw == nil {
+		return nil
+	}
+	if err := printOffice(pw); err != nil {
+		return err
+	}
+	_, err := fmt.Fprintln(os.Stdout)
+	return err
+}
+
+func printOffice(pw *passwdInfo) error {
+	if pw.office == "" && pw.phone == "" {
+		return nil
+	}
+	var line string
+	if pw.office != "" && pw.phone != "" {
+		line = fmt.Sprintf("Office: %s, %s", pw.office, pw.phone)
+	} else if pw.office != "" {
+		line = fmt.Sprintf("Office: %s", pw.office)
+	} else {
+		line = fmt.Sprintf("Office: %s", pw.phone)
 	}
 	_, err := fmt.Fprintln(os.Stdout, line)
 	return err
@@ -182,4 +271,29 @@ func lookupGecos(user string) string {
 	gecos := C.GoString(pw.pw_gecos)
 	name, _, _ := strings.Cut(gecos, ",")
 	return name
+}
+
+func lookupPasswd(user string) *passwdInfo {
+	cuser := C.CString(user)
+	defer C.free(unsafe.Pointer(cuser))
+	pw := C.getpwnam(cuser)
+	if pw == nil {
+		return nil
+	}
+	gecos := C.GoString(pw.pw_gecos)
+	fields := strings.SplitN(gecos, ",", 4)
+	info := &passwdInfo{
+		dir:   C.GoString(pw.pw_dir),
+		shell: C.GoString(pw.pw_shell),
+	}
+	if len(fields) > 0 {
+		info.name = fields[0]
+	}
+	if len(fields) > 1 {
+		info.office = fields[1]
+	}
+	if len(fields) > 2 {
+		info.phone = fields[2]
+	}
+	return info
 }
