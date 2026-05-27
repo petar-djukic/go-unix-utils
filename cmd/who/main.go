bc5a8ac557fc0c028e17c01dd8a8d8de7646fe32
Petar Djukic <petar.djukic@gmail.com>
2026-05-27 10:29:44 -0400

    Task 5583: [stitch] cmd/who implementation -- srd097-who R3.1, R3.2, R3.3
    Tokens-Input: 291208
    Tokens-Output: 919
    Tokens-Cache-Creation: 216687
    Tokens-Cache-Read: 74514
    Tokens-Cost-USD: 1.8042
    Loc-Prod-Before: 26478
    Loc-Prod-After: 26478
    Loc-Test-Before: 23023
    Loc-Test-After: 23023
    Duration-Seconds: 69

--- cmd/who/main.go
+++ cmd/who/main.go
@@ -1,7 +1,7 @@
 // Copyright (c) 2026 Petar Djukic. All rights reserved.
 // SPDX-License-Identifier: MIT
 
-// Implements srd097-who R1.1, R1.2, R1.3, R1.4, R2.1, R2.2, R2.3, R2.4.
+// Implements srd097-who R1.1, R1.2, R1.3, R1.4, R2.1, R2.2, R2.3, R2.4, R3.1, R3.2, R3.3.
 package main
 
 /*
