8115f801653e65b67f214c5a76cd15ef3cf03ce6
Petar Djukic <petar.djukic@gmail.com>
2026-05-27 10:28:06 -0400

    Task 5582: [stitch] cmd/who implementation -- srd097-who R2.1, R2.2, R2.3, R2.4
    Tokens-Input: 481898
    Tokens-Output: 18202
    Tokens-Cache-Creation: 236290
    Tokens-Cache-Read: 245597
    Tokens-Cost-USD: 2.4693
    Loc-Prod-Before: 26338
    Loc-Prod-After: 26478
    Loc-Test-Before: 23023
    Loc-Test-After: 23023
    Duration-Seconds: 325

--- cmd/who/main.go
+++ cmd/who/main.go
@@ -1,7 +1,7 @@
 // Copyright (c) 2026 Petar Djukic. All rights reserved.
 // SPDX-License-Identifier: MIT
 
-// Implements srd097-who R1.1, R1.2, R1.3, R1.4.
+// Implements srd097-who R1.1, R1.2, R1.3, R1.4, R2.1, R2.2, R2.3, R2.4.
 package main
 
 /*
@@ -24,6 +24,10 @@ import (
 const helpText = `Usage: who [OPTION]... [ FILE | ARG1 ARG2 ]
 Print information about users who are currently logged in.
 
+  -b, --boot     time of last system boot
+  -H, --heading  print line of column headings
+  -q, --count    all login names and number of users logged on
+  -u, --users    list users logged in
       --help     display this help and exit
       --version  output version information and exit
 
@@ -33,6 +37,13 @@ If ARG1 ARG2 given, -m presumed: 'am i' or 'mom likes' are usual.
 
 const versionText = `who (go-unix-utils) dev
 `
+
+var (
+	showHeading bool
+	showIdle    bool
+	showBoot    bool
+	showCount   bool
+)
 
 type whoEntry struct {
 	user string
@@ -52,10 +63,30 @@ func main() {
 		}
 	}
 
-	entries := readEntries(amI)
-	for _, e := range entries {
-		if err := printEntry(e); err != nil {
+	if showCount {
+		if err := printCount(amI); err != nil {
 			os.Exit(1)
+		}
+		return
+	}
+
+	printUsers := showIdle || !showBoot
+	if showHeading {
+		if err := printHeader(printUsers); err != nil {
+			os.Exit(1)
+		}
+	}
+	if showBoot {
+		if err := printBootEntry(); err != nil {
+			os.Exit(1)
+		}
+	}
+	if printUsers {
+		entries := readEntries(amI)
+		for _, e := range entries {
+			if err := printEntry(e); err != nil {
+				os.Exit(1)
+			}
 		}
 	}
 }
@@ -110,6 +141,25 @@ func readEntries(amI bool) []whoEntry {
 	return entries
 }
 
+func readBootTime() (time.Time, bool) {
+	C.setutxent()
+	defer C.endutxent()
+	var bootTime time.Time
+	found := false
+	for {
+		entry := C.getutxent()
+		if entry == nil {
+			break
+		}
+		if entry.ut_type != C.BOOT_TIME {
+			continue
+		}
+		bootTime = time.Unix(int64(entry.ut_tv.tv_sec), 0)
+		found = true
+	}
+	return bootTime, found
+}
+
 func currentLine() string {
 	tty := C.ttyname(C.int(os.Stdin.Fd()))
 	if tty == nil {
@@ -118,8 +168,22 @@ func currentLine() string {
 	return strings.TrimPrefix(C.GoString(tty), "/dev/")
 }
 
+func printHeader(users bool) error {
+	var hdr string
+	if users && showIdle {
+		hdr = fmt.Sprintf("%-8s %-12s %-16s %-6s%s", "NAME", "LINE", "TIME", "IDLE", "COMMENT")
+	} else {
+		hdr = fmt.Sprintf("%-8s %-12s %-16s %s", "NAME", "LINE", "TIME", "COMMENT")
+	}
+	_, err := fmt.Fprintln(os.Stdout, hdr)
+	return err
+}
+
 func printEntry(e whoEntry) error {
 	timeStr := e.time.Format("2006-01-02 15:04")
+	if showIdle {
+		return printEntryWithIdle(e, timeStr)
+	}
 	var line string
 	if e.host != "" {
 		line = fmt.Sprintf("%-8s %-12s %s (%s)", e.user, e.line, timeStr, e.host)
@@ -127,6 +191,57 @@ func printEntry(e whoEntry) error {
 		line = fmt.Sprintf("%-8s %-12s %s", e.user, e.line, timeStr)
 	}
 	_, err := fmt.Fprintln(os.Stdout, line)
+	return err
+}
+
+func printEntryWithIdle(e whoEntry, timeStr string) error {
+	idle := idleString(e.line)
+	var line string
+	if e.host != "" {
+		line = fmt.Sprintf("%-8s %-12s %s %5s (%s)", e.user, e.line, timeStr, idle, e.host)
+	} else {
+		line = fmt.Sprintf("%-8s %-12s %s %5s", e.user, e.line, timeStr, idle)
+	}
+	_, err := fmt.Fprintln(os.Stdout, line)
+	return err
+}
+
+func idleString(ttyName string) string {
+	fi, err := sys.Stat("/dev/" + ttyName)
+	if err != nil {
+		return "?"
+	}
+	secs := int(time.Since(fi.AccessTime).Seconds())
+	if secs < 60 {
+		return "."
+	}
+	if secs >= 24*60*60 {
+		return "old"
+	}
+	return fmt.Sprintf("%02d:%02d", secs/3600, (secs%3600)/60)
+}
+
+func printBootEntry() error {
+	bootTime, found := readBootTime()
+	if !found {
+		return nil
+	}
+	timeStr := bootTime.Format("2006-01-02 15:04")
+	line := fmt.Sprintf("%-8s %-12s %s", "", "system boot", timeStr)
+	_, err := fmt.Fprintln(os.Stdout, line)
+	return err
+}
+
+func printCount(amI bool) error {
+	entries := readEntries(amI)
+	names := make([]string, len(entries))
+	for i, e := range entries {
+		names[i] = e.user
+	}
+	if _, err := fmt.Fprintln(os.Stdout, strings.Join(names, " ")); err != nil {
+		return err
+	}
+	_, err := fmt.Fprintf(os.Stdout, "# users=%d\n", len(entries))
 	return err
 }
 
@@ -145,6 +260,14 @@ func parseArgs(args []string) ([]string, bool) {
 		case arg == "--version":
 			fmt.Fprint(os.Stdout, versionText)
 			os.Exit(0)
+		case arg == "--heading":
+			showHeading = true
+		case arg == "--users":
+			showIdle = true
+		case arg == "--boot":
+			showBoot = true
+		case arg == "--count":
+			showCount = true
 		case arg == "--":
 			endOfFlags = true
 		case strings.HasPrefix(arg, "--"):
@@ -152,9 +275,7 @@ func parseArgs(args []string) ([]string, bool) {
 			fmt.Fprintln(os.Stderr, "Try 'who --help' for more information.")
 			os.Exit(1)
 		case strings.HasPrefix(arg, "-") && len(arg) > 1:
-			fmt.Fprintf(os.Stderr, "who: invalid option -- '%c'\n", arg[1])
-			fmt.Fprintln(os.Stderr, "Try 'who --help' for more information.")
-			os.Exit(1)
+			parseShortFlags(arg[1:])
 		default:
 			remaining = append(remaining, arg)
 		}
@@ -168,4 +289,23 @@ func parseArgs(args []string) ([]string, bool) {
 		os.Exit(1)
 	}
 	return remaining, false
+}
+
+func parseShortFlags(flags string) {
+	for _, ch := range flags {
+		switch ch {
+		case 'H':
+			showHeading = true
+		case 'u':
+			showIdle = true
+		case 'b':
+			showBoot = true
+		case 'q':
+			showCount = true
+		default:
+			fmt.Fprintf(os.Stderr, "who: invalid option -- '%c'\n", ch)
+			fmt.Fprintln(os.Stderr, "Try 'who --help' for more information.")
+			os.Exit(1)
+		}
+	}
 }
