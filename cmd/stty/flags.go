a1a9df53c9ab07a67be2cadfd50294c3206dbca7
Petar Djukic <petar.djukic@gmail.com>
2026-05-29 11:58:01 -0400

    Task 5645: [stitch] cmd/stty implementation (srd105-stty R5.1, R6.1, R6.2, R7.1)
    Tokens-Input: 841463
    Tokens-Output: 27063
    Tokens-Cache-Creation: 258139
    Tokens-Cache-Read: 583308
    Tokens-Cost-USD: 2.7999
    Loc-Prod-Before: 31856
    Loc-Prod-After: 32018
    Loc-Test-Before: 24673
    Loc-Test-After: 24685
    Duration-Seconds: 538

--- cmd/stty/flags.go
+++ cmd/stty/flags.go
@@ -90,3 +90,10 @@ var controlChars = []ccDef{
 	{"time", unix.VTIME, 0, true},
 	{"status", unix.VSTATUS, 0x14, false},
 }
+
+var validBaudRates = map[uint64]bool{
+	0: true, 50: true, 75: true, 110: true, 134: true, 150: true,
+	200: true, 300: true, 600: true, 1200: true, 1800: true,
+	2400: true, 4800: true, 9600: true, 19200: true, 38400: true,
+	57600: true, 115200: true, 230400: true,
+}
--- cmd/stty/settings.go
+++ cmd/stty/settings.go
@@ -12,17 +12,23 @@ import (
 )
 
 func applySettings(fd int, t *unix.Termios, settings []string) error {
-	for _, s := range settings {
-		if err := applySetting(t, s); err != nil {
+	for i := 0; i < len(settings); i++ {
+		n, err := applySetting(t, settings[i:])
+		if err != nil {
 			return err
 		}
+		i += n - 1
 	}
 	return unix.IoctlSetTermios(fd, unix.TIOCSETA, t)
 }
 
-func applySetting(t *unix.Termios, s string) error {
+func applySetting(t *unix.Termios, args []string) (int, error) {
+	s := args[0]
 	if strings.Contains(s, ":") {
-		return restoreFromSave(t, s)
+		return 1, restoreFromSave(t, s)
+	}
+	if s == "ispeed" || s == "ospeed" {
+		return applySpeedKeyword(t, s, args[1:])
 	}
 	negate := strings.HasPrefix(s, "-")
 	name := s
@@ -30,12 +36,26 @@ func applySetting(t *unix.Termios, s string) error {
 		name = s[1:]
 	}
 	if applyFlag(t, name, negate) {
-		return nil
+		return 1, nil
 	}
 	if !negate && applyCSize(t, name) {
-		return nil
+		return 1, nil
 	}
-	return fmt.Errorf("invalid argument '%s'", s)
+	if cc, ok := findCC(name); ok && !negate {
+		return applyCharSetting(t, cc, args[1:])
+	}
+	if applyCombo(t, s) {
+		return 1, nil
+	}
+	if n, err := strconv.ParseUint(s, 10, 64); err == nil {
+		if !validBaudRates[n] {
+			return 0, fmt.Errorf("invalid argument '%s'", s)
+		}
+		t.Ispeed = n
+		t.Ospeed = n
+		return 1, nil
+	}
+	return 0, fmt.Errorf("invalid argument '%s'", s)
 }
 
 func applyFlag(t *unix.Termios, name string, negate bool) bool {
@@ -125,4 +145,139 @@ func parseHexFields(parts []string) ([]uint64, error) {
 		vals[i] = v
 	}
 	return vals, nil
+}
+
+func findCC(name string) (ccDef, bool) {
+	for _, c := range controlChars {
+		if c.name == name {
+			return c, true
+		}
+	}
+	return ccDef{}, false
+}
+
+func applyCharSetting(t *unix.Termios, cc ccDef, rest []string) (int, error) {
+	if len(rest) < 1 {
+		return 0, fmt.Errorf("missing argument to '%s'", cc.name)
+	}
+	val, err := parseCharValue(rest[0])
+	if err != nil {
+		return 0, err
+	}
+	t.Cc[cc.index] = val
+	return 2, nil
+}
+
+func parseCharValue(s string) (uint8, error) {
+	if s == "^-" || s == "undef" {
+		return vdisable, nil
+	}
+	if len(s) == 2 && s[0] == '^' {
+		if s[1] == '?' {
+			return 0x7f, nil
+		}
+		return s[1] & 0x1f, nil
+	}
+	if len(s) == 1 {
+		return s[0], nil
+	}
+	n, err := strconv.ParseUint(s, 0, 8)
+	if err != nil {
+		return 0, fmt.Errorf("invalid integer argument '%s'", s)
+	}
+	return uint8(n), nil
+}
+
+func applySpeedKeyword(t *unix.Termios, keyword string, rest []string) (int, error) {
+	if len(rest) < 1 {
+		return 0, fmt.Errorf("missing argument to '%s'", keyword)
+	}
+	n, err := strconv.ParseUint(rest[0], 10, 64)
+	if err != nil || !validBaudRates[n] {
+		return 0, fmt.Errorf("invalid argument '%s'", rest[0])
+	}
+	if keyword == "ispeed" {
+		t.Ispeed = n
+	} else {
+		t.Ospeed = n
+	}
+	return 2, nil
+}
+
+func applyCombo(t *unix.Termios, s string) bool {
+	switch s {
+	case "sane":
+		applySane(t)
+	case "raw":
+		applyRaw(t)
+	case "cooked", "-raw":
+		applyCooked(t)
+	case "evenp", "parity":
+		applyEvenParity(t)
+	case "-evenp", "-parity":
+		clearParity(t)
+	case "oddp":
+		applyOddParity(t)
+	case "-oddp":
+		clearOddParity(t)
+	default:
+		return false
+	}
+	return true
+}
+
+func applySane(t *unix.Termios) {
+	setFlagsSane(&t.Cflag, cflags)
+	setFlagsSane(&t.Iflag, iflags)
+	setFlagsSane(&t.Oflag, oflags)
+	setFlagsSane(&t.Lflag, lflags)
+	t.Cflag = (t.Cflag &^ unix.CSIZE) | unix.CS8
+	for _, c := range controlChars {
+		t.Cc[c.index] = c.saneVal
+	}
+}
+
+func setFlagsSane(field *uint64, flags []flagDef) {
+	for _, f := range flags {
+		if f.sane == 1 {
+			*field |= f.mask
+		} else if f.sane == -1 {
+			*field &^= f.mask
+		}
+	}
+}
+
+func applyRaw(t *unix.Termios) {
+	t.Iflag &^= unix.IGNBRK | unix.BRKINT | unix.PARMRK |
+		unix.ISTRIP | unix.INLCR | unix.IGNCR | unix.ICRNL | unix.IXON
+	t.Oflag &^= unix.OPOST
+	t.Lflag &^= unix.ECHO | unix.ECHONL | unix.ICANON |
+		unix.ISIG | unix.IEXTEN
+	t.Cflag = (t.Cflag &^ (unix.CSIZE | unix.PARENB)) | unix.CS8
+	t.Cc[unix.VMIN] = 1
+	t.Cc[unix.VTIME] = 0
+}
+
+func applyCooked(t *unix.Termios) {
+	t.Iflag |= unix.BRKINT | unix.IGNPAR | unix.ISTRIP |
+		unix.ICRNL | unix.IXON
+	t.Oflag |= unix.OPOST
+	t.Lflag |= unix.ISIG | unix.ICANON
+	t.Cc[unix.VEOF] = 0x04
+}
+
+func applyEvenParity(t *unix.Termios) {
+	t.Cflag = (t.Cflag &^ (unix.CSIZE | unix.PARODD)) | unix.CS7 | unix.PARENB
+}
+
+func clearParity(t *unix.Termios) {
+	t.Cflag = (t.Cflag &^ (unix.CSIZE | unix.PARENB)) | unix.CS8
+}
+
+func applyOddParity(t *unix.Termios) {
+	t.Cflag = (t.Cflag &^ unix.CSIZE) | unix.CS7 | unix.PARENB | unix.PARODD
+}
+
+func clearOddParity(t *unix.Termios) {
+	t.Cflag = (t.Cflag &^ (unix.CSIZE | unix.PARENB | unix.PARODD)) | unix.CS8
 }
